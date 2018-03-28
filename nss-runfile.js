9a8a56276890a77b38f94943f5377c668e6a9705
Florian Krauthan <mail@fkrauthan.de>
2018-03-28 00:05:48 -0700

    Added the docs to the repository

--- nss-runfile.js
+++ nss-runfile.js
@@ -11,7 +11,7 @@ nssRun.task('lint:nss-runfile', () => nssRun.run('eslint ./nss-runfile.js'));
 
 
 /**
- * Doc geneartion task
+ * Doc generation task
  */
 nssRun.task('doc', () => nssRun.run('jsdoc -d docs -R ./README.md ./src/index.js'), { description: 'Generates the plugin docs' });
 
